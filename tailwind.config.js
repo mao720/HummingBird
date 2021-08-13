38c6985b6006d7a902868bcc4a9adb7939dda8c8
maojunfeng <1009935357@qq.com>
2021-08-13 09:27:30 +0800

    v1.0.5-code styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
     purge: ['./pages/**/*.ts', './pages/**/*.tsx', './components/**/*.ts', './components/**/*.tsx', './styles/**/*.css'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        pre: {
+                            'min-width': '100%',
+                            width: 0,
+                        }
+                    }
+                }
+            }
+        },
         fontFamily: {
             'kai': '楷体, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
         },
